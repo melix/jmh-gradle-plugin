a4cd928ec3c42f1afe1d81375e62111c58c98e0e
Dmitry Vyazelenko <vyazelenko@yahoo.com>
2015-12-17 23:06:00 +0100

    Apply same set of META-INF excludes in all cases

--- src/main/groovy/me/champeau/gradle/JMHPlugin.groovy
+++ src/main/groovy/me/champeau/gradle/JMHPlugin.groovy
@@ -77,25 +77,26 @@ class JMHPlugin implements Plugin<Project> {
             destinationDir = jmhGeneratedClassesDir
         }
 
+        def metaInfExcludes = ['META-INF/*.SF', 'META-INF/*.DSA', 'META-INF/*.RSA']
         if (project.plugins.findPlugin('com.github.johnrengelman.shadow') == null) {
             project.tasks.create(name: 'jmhJar', type: Jar) {
                 dependsOn 'jmhCompileGenerateClasses'
                 inputs.dir project.sourceSets.jmh.output
                 doFirst {
                     def filter = { it.isDirectory() ? it : project.zipTree(it) }
-                    def exclusions = {
-                        exclude '**/META-INF/services/**'
-                        exclude '**/META-INF/*.SF'
-                        exclude '**/META-INF/*.DSA'
-                        exclude '**/META-INF/*.RSA'
+                    from(project.configurations.jmh.collect(filter)) {
+                        exclude metaInfExcludes
                     }
-                    from(project.configurations.jmh.collect(filter), exclusions)
-                    from(project.configurations.runtime.collect(filter), exclusions)
+                    from(project.configurations.runtime.collect(filter)) {
+                        exclude metaInfExcludes
+                    }
                     from(project.sourceSets.jmh.output)
                     from(project.sourceSets.main.output)
                     from(project.file(jmhGeneratedClassesDir))
                     if (extension.includeTests) {
-                        from(project.configurations.testRuntime.collect(filter), exclusions)
+                        from(project.configurations.testRuntime.collect(filter)) {
+                            exclude metaInfExcludes
+                        }
                         from(project.sourceSets.test.output)
                     }
                 }
@@ -139,7 +140,7 @@ class JMHPlugin implements Plugin<Project> {
             shadow.from(project.file(jmhGeneratedClassesDir))
 
             shadow.configurations = [project.configurations.runtime, project.configurations.jmh]
-            shadow.exclude('META-INF/INDEX.LIST', 'META-INF/*.SF', 'META-INF/*.DSA', 'META-INF/*.RSA')
+            shadow.exclude(metaInfExcludes)
         }
 
         project.tasks.create(name: 'jmh', type: JavaExec) {
