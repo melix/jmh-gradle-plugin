e5a31f0e686983c064e3a4a522a7a057b1a34d4c
aalmiray <aalmiray@gmail.com>
2016-04-28 10:58:38 +0200

    Code cleanup

--- src/main/groovy/me/champeau/gradle/JMHPluginExtension.java
+++ src/main/groovy/me/champeau/gradle/JMHPluginExtension.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.util.*;
 
 import static org.codehaus.groovy.runtime.DefaultGroovyMethods.join;
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.unique;
 
 public class JMHPluginExtension {
     private final Project project;
@@ -112,7 +111,7 @@ public class JMHPluginExtension {
         if (benchmarkMode == null || benchmarkMode.isEmpty()) {
             return null;
         }
-        return join((Iterable<String>) new LinkedHashSet<String>(benchmarkMode), ",");
+        return join(benchmarkMode, ",");
     }
 
     private void resolveArgs() {
