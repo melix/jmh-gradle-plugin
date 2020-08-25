3fbe2dadb989101fa6f7ea8250c92e1e49d574fd
Stefan Oehme <st.oehme@gmail.com>
2020-08-25 19:04:16 +0200

    Fix file descriptor leak through worker classpath
    Workers created through the worker API live as long as the Gradle daemon.
    Therefor, they should not have any JARs on their classpath that are part
    of the project being built. Otherwise, the worker keeps these JARs open,
    leading to a file descriptor leak and an unability to clean the project
    on Windows. Also, on every change to the dependencies, a new worker would
    be created, greatly increasing memory pressure on the system. Finally,
    the workers of different subprojects would be incompatible due to their
    different classpaths, increasing the number of spwawned workers even more.
    
    This fix changes the worker such that only JMH is put on the worker classpath.
    The project's own dependencies are passed to the worker as arguments.
    Some trickery was necessary, because JMH insists on looking for project-specific
    files on its own classpath. This should probably be reported as a bug to JMH.
    It should either look on the Thread's context classloader (which we could change)
    or make these locations externally configurable on the Runner class.

--- src/main/groovy/me/champeau/gradle/IsolatedRunner.java
+++ src/main/groovy/me/champeau/gradle/IsolatedRunner.java
@@ -16,31 +16,39 @@
 package me.champeau.gradle;
 
 import org.gradle.api.GradleException;
+import org.openjdk.jmh.runner.BenchmarkList;
+import org.openjdk.jmh.runner.CompilerHints;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Set;
 
 public class IsolatedRunner implements Runnable {
 
     private final Options options;
-    private Set<File> classpathUnderTest;
+    private final Set<File> classpathUnderTest;
+    private final File benchmarkList;
+    private final File compilerHints;
 
     @Inject
-    public IsolatedRunner(final Options options, final Set<File> classpathUnderTest) {
+    public IsolatedRunner(final Options options, final Set<File> classpathUnderTest, File benchmarkList, File compilerHints) {
         this.options = options;
         this.classpathUnderTest = classpathUnderTest;
+        this.benchmarkList = benchmarkList;
+        this.compilerHints = compilerHints;
     }
 
     @Override
     public void run() {
         String originalClasspath = System.getProperty("java.class.path");
         Runner runner = new Runner(options);
+        updateBenchmarkList(runner);
+        updateCompilerHints();
         try {
             System.setProperty("java.class.path", toPath(classpathUnderTest));
             // JMH uses the system property java.class.path to derive the runtime classpath of the forked JVM
@@ -55,6 +63,34 @@ public class IsolatedRunner implements Runnable {
                 System.clearProperty("java.class.path");
             }
         }
+    }
+
+    private void updateBenchmarkList(Runner runner) {
+        BenchmarkList benchmarkList = BenchmarkList.fromFile(this.benchmarkList.getAbsolutePath());
+        try {
+            Field listField = runner.getClass().getDeclaredField("list");
+            makeWriteable(listField);
+            listField.set(runner, benchmarkList);
+        } catch (Exception e) {
+            throw new GradleException("Error instantiating benchmarks", e);
+        }
+    }
+    private void updateCompilerHints() {
+        CompilerHints compilerHints = CompilerHints.fromFile(this.compilerHints.getAbsolutePath());
+        try {
+            Field listField = CompilerHints.class.getDeclaredField("defaultList");
+            makeWriteable(listField);
+            listField.set(null, compilerHints);
+        } catch (Exception e) {
+            throw new GradleException("Error instantiating benchmarks", e);
+        }
+    }
+
+    private void makeWriteable(Field listField) throws NoSuchFieldException, IllegalAccessException {
+        listField.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(listField, listField.getModifiers() & ~Modifier.FINAL);
     }
 
     private String toPath(Set<File> classpathUnderTest) {
