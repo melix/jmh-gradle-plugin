4a7ad8ac9d088d1e64412a651b5be18e69f24b93
Goooler <wangzongler@gmail.com>
2023-07-31 19:53:21 +0800

    Migrate to convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ plugins {
     id("org.nosphere.apache.rat") version "0.8.0"
     id("net.nemerosa.versioning") version "3.0.0"
     id("com.github.kt3k.coveralls") version "2.12.2"
+    id("me.champeau.convention-test")
+    id("me.champeau.convention-funcTest")
     id("me.champeau.plugin-configuration")
     id("jacoco")
     id("groovy")
@@ -28,9 +30,6 @@ buildScanRecipes {
     recipes("git-status", "travis-ci")
     recipe(mapOf("baseUrl" to "https://github.com/melix/jmh-gradle-plugin/tree"), "git-commit")
 }
-
-apply(from = "gradle/test.gradle")
-apply(from = "gradle/funcTest.gradle")
 
 val jmhVersion: String by project
 val spockVersion: String by project
@@ -43,7 +42,7 @@ dependencies {
     testImplementation("org.spockframework:spock-core:$spockVersion") {
         exclude(mapOf("group" to "org.codehaus.groovy"))
     }
-    "pluginsUnderTest"("gradle.plugin.com.github.johnrengelman:shadow:$shadowVersion")
+    pluginsUnderTest("gradle.plugin.com.github.johnrengelman:shadow:$shadowVersion")
 
     testImplementation("org.openjdk.jmh:jmh-core:$jmhVersion")
     testImplementation("org.openjdk.jmh:jmh-generator-bytecode:$jmhVersion")
