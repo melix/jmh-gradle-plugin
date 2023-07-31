4cfc127aab9e60ecdd1a49385ea287f243ebb94d
Goooler <wangzongler@gmail.com>
2023-07-31 20:27:04 +0800

    Minor cleanups
    * Remove me.champeau.gradle.jmh.properties
    
    * Remove extra groovy plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@
  */
 
 plugins {
+    jacoco
     id("me.champeau.buildscan-recipes") version "0.2.3"
     id("org.nosphere.apache.rat") version "0.8.0"
     id("net.nemerosa.versioning") version "3.0.0"
@@ -22,8 +23,6 @@ plugins {
     id("me.champeau.convention-test")
     id("me.champeau.convention-funcTest")
     id("me.champeau.plugin-configuration")
-    id("jacoco")
-    id("groovy")
 }
 
 buildScanRecipes {
